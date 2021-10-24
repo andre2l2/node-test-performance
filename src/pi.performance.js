a132a158cfb181597e80687f3114eddaf8a4aa2f
Andre Ferreira <delcferreira@gmail.com>
2021-10-24 18:15:34 -0300

    :tada: Initial commit

---
+++ src/pi.performance.js
@@ -0,0 +1,31 @@
+const pi = Math.PI;
+const piConstant = 4;
+
+/**
+ *
+ * @param { decimalPlace } number
+ * Is the length of the pi, for example 3.1415 have a 4 decimalPlace.
+ */
+function piTest(decimalPlace = 8) {
+	let contollerCounter = 0;
+	let result = 0;
+	let divider = 1;
+
+	console.time();
+
+	while (result.toFixed(decimalPlace) !== pi.toFixed(decimalPlace)) {
+		if (contollerCounter % 2 === 0) {
+			result += piConstant / divider;
+		} else {
+			result -= piConstant / divider;
+		}
+
+		divider += 2;
+		contollerCounter++;
+	}
+
+	console.timeEnd();
+	console.log(result.toFixed(decimalPlace));
+}
+
+module.exports = piTest;
